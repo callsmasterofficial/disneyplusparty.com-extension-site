db80a3e72bc0c95c8ef98db32a50be3af5fb2c21
NikhilRajput <nikhilcallsmaster12@gmail.com>
2023-09-26 14:46:56 -0700

    SDfsd

--- siteConfig.js
+++ siteConfig.js
@@ -1,29 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
   siteName: 'Disney Plus Party',
-  url:"https://www.disneyplusparty.com",
-  blogUrl: "/blog",
-  siteId: "62a7919492c68b0d2f1e4078",
+  url: 'https://www.disneyplusparty.com',
+  blogUrl: '/blog',
+  siteId: '62a7919492c68b0d2f1e4078',
   extLink: {
     chrome:
-    'https://chrome.google.com/webstore/detail/disney-plus-watch-party/nkciggljdfnkpcdngdohlpelojmklhin',
-  firefox: 'https://addons.mozilla.org/addon/disney-plus-watch-party/',
-  edge: 'https://microsoftedge.microsoft.com/addons/detail/disney-plus-watch-party/pdgkibicekgmlijcbnpenofoigefjdfd',
-},
+      'https://chrome.google.com/webstore/detail/disney-plus-watch-party/nkciggljdfnkpcdngdohlpelojmklhin',
+    firefox: 'https://addons.mozilla.org/addon/disney-plus-watch-party/',
+    edge: 'https://microsoftedge.microsoft.com/addons/detail/disney-plus-watch-party/pdgkibicekgmlijcbnpenofoigefjdfd',
+  },
   logo: '/logo.png',
-  gtagId:"211032763-3",
+  gtagId: '211032763-3',
   section: {
     first: {
       heading: 'Co-watch Disney with your favorite people, anytime, anywhere!',
       html: `
-      Disney is more fun with friends and family. Then why watch it alone? Now you can watch Disney with your favorite people while staying anywhere across the world. Install the <a href="https://www.disneyplusparty.com"><b>Disney Plus Party</b></a> extension and have online watch parties in perfect sync!
+      Disney is more fun with friends and family. Then why watch it alone? Now you can watch Disney with your favorite people while staying anywhere across the world. Install the <a href="https://www.disneyplusparty.com"><b>Disney Plus Party</b></a> extension and have online watch parties in perfect sync!<br/><br/>Moreover, Disney Plus even offers GroupWatch, which allows users worldwide to enjoy shows, series, and movies with friends and family. Don’t worry; even Apple users can use SharePlay for this. And to make your Disney Plus Group Watch special, you can use its feature. Its features provide seamless viewing, allowing continuous playback and control over rewinding, pausing, and fast-forwarding. Therefore, here are guidelines for setting up these features are available below. Note that Disney Plus Group Watch is currently in the US. But iPhone and iPad users can enjoy Disney+ with friends through SharePlay.
       `,
       video:
         'https://super-dashboard-images-cdn.s3.amazonaws.com/videos/1646197313207-sync-disney-plus.mp4',
     },
     second: {
       heading: 'How to use the Disney Plus Party?',
-      subHeading:
-        '',
+      subHeading: `If you're new to the Disney Plus party scene and want to discover how to host a watch party on Disney Plus, you've come to the right spot. Here, we provide a step-by-step guide to help you kickstart your Disney Plus party experience, ensuring a fun and enjoyable viewing time with friends and family. So, let’s get started:`,
       video:
         'https://super-dashboard-images-cdn.s3.amazonaws.com/videos/1645708727258-how-to-use-disney-plus-watch-party-final.mp4.mp4',
       accordian: [
@@ -68,46 +68,44 @@ export default {
           answer:
             'Visit the watch party link shared with you by your friend. If you have not installed the extension, you will be required to install it and log in to the Disney account.',
         },
-
       ],
     },
     three: {
       heading: 'Features of Disney Plus Watch Party',
-      subHeading:
-        '',
+      subHeading: `Similar to other streaming platforms, Disney Plus Party offers a range of exceptional features that can elevate your movie night experience. Installing the Disney Plus Watch Party extension enables access to these exciting features. Explore the information below to delve into the features of Disney Plus Watch Party: `,
       video:
         'https://super-dashboard-images-cdn.s3.amazonaws.com/videos/1646197414484-disney-plus-watch-party-avtar.mp4',
       accordian: [
         {
-          id: "t1",
+          id: 't1',
           question: 'Watch Disney in sync',
           answer:
             'One of the greatest features of the extension is, it helps you watch Disney shows in perfect synchronization with people from all across the world.',
         },
 
         {
-          id: "t2",
+          id: 't2',
           question: 'User-friendly',
           answer:
             'This is a user-friendly extension that anybody can use to create or join the watch parties easily.',
         },
 
         {
-          id: "t3",
+          id: 't3',
           question: 'Live Chat',
           answer:
             'The chat feature of the extension enables the users to text or share emojis with each other while watching the shows.',
         },
 
         {
-          id: "t4",
+          id: 't4',
           question: 'Customize the Disney Plus Party profile',
           answer:
             'Customize your watch party profile. Use funny avatars of your choice as your profile icon. In addition to that, choose your username or nickname. The username and icon will be visible to your friends.',
         },
 
         {
-          id: "t5",
+          id: 't5',
           question: 'Worldwide access',
           answer:
             'Disney Watch Party extension is available all across the world. Therefore, Disney subscribers living in different locations can install and use the extension from anywhere.',
@@ -118,21 +116,21 @@ export default {
   faq: {
     accordian: [
       {
-        id: "f1",
+        id: 'f1',
         question: 'What is the use of the Disney Plus Party extension? ',
         answer:
           'The extension is a helpful tool for all the Disney Plus subscribers who live far away from their loved ones and miss watching Disney Plus shows together. With the Disney Plus Party extension, you can create or join online watch parties with people from different locations. ',
       },
 
       {
-        id: "f2",
+        id: 'f2',
         question: 'How to install the extension? ',
         answer:
           'You can install the extension by clicking here. You will be redirected to the installation page, where you need to select the “Add to Chrome” option and then tap on the “Add Extension” button. However, before proceeding with the installation procedure, please make sure that you use Google Chrome or Microsoft Edge browsers. In addition to that, also remember that the extension only works with the Chromebook, Windows, or macOS laptop and PC.',
       },
 
       {
-        id: "f3",
+        id: 'f3',
         question: 'Can I also chat with friends while streaming?',
         answer:
           'Yes, you can chat with your friends during the watch party. The Disney Watch Party opens a sidebar while streaming the shows in sync. Thus, the users can chat with each other during the watch party.',
@@ -144,7 +142,7 @@ export default {
       },
 
       {
-        id: "f4",
+        id: 'f4',
         question: 'What are the steps to host a watch party? ',
         answer:
           'Please follow the below steps to host or create the virtual party via this extension:- Install and add the extension to the toolbar for your browser. ',
@@ -156,8 +154,9 @@ export default {
       },
 
       {
-        id: "f5",
-        question: 'What are the requirements to install the Disney Plus Party extension? ',
+        id: 'f5',
+        question:
+          'What are the requirements to install the Disney Plus Party extension? ',
         answer:
           'The users must remember the following points before installing the extension: –',
         p1: 'They must have a stable internet connection.',
@@ -166,16 +165,17 @@ export default {
         p4: 'The users must have a Disney Plus subscription.',
         p5: '',
       },
-      
-   
     ],
   },
   footer: {
-      subheading: "Get it, Its Free!",
-      title: "I'm ready, Give me Disney Party?",
-      tags:[ "Disney Plus Party", "Disney Plus Watch Party",
-        "Watch Party Disney Plus, How to do a Watch Party on Disney Plus, Disney Watch Party, Disney Party, Watch Party on Disney Plus, How to Host a Disney Plus Watch Party",
-      ],
-      disclaimer:"Disney Plus is a trademark of Walt Disney Company. This website & extension hold no association or affiliation with Disney or any third-party companies."
+    subheading: 'Get it, Its Free!',
+    title: "I'm ready, Give me Disney Party?",
+    tags: [
+      'Disney Plus Party',
+      'Disney Plus Watch Party',
+      'Watch Party Disney Plus, How to do a Watch Party on Disney Plus, Disney Watch Party, Disney Party, Watch Party on Disney Plus, How to Host a Disney Plus Watch Party',
+    ],
+    disclaimer:
+      'Disney Plus is a trademark of Walt Disney Company. This website & extension hold no association or affiliation with Disney or any third-party companies.',
   },
 }
