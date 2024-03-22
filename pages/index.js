8e20a33c63c2fc5b76fa9f78ccf1b34c5c7a675b
NikhilRajput <nikhilcallsmaster12@gmail.com>
2024-03-22 15:35:26 +0530

    mashgfa

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,19 @@ import Text2 from '../components/Text2'
 import Fask_ques from '../components/Fask_ques'
 import Footer from '../components/Footer'
 import Script from 'next/script'
+import { getAllmeta, getHome } from '../locale/index'
 
-export async function getStaticProps() {
+export async function getStaticProps({ locale }) {
+  const home = getHome(locale)
+  const allmeta = getAllmeta(locale)
   return {
     props: {
+      home,
+      allmeta,
       meta: {
-        title:
-          'Disney Plus Party - Watch Disney Plus with Friends Online from Anywhere!',
-        description:
-          'Disney Plus Party allows you to stream Disney Plus with your friends & family online from different locations. Install the Extension Now!',
-        keywords:
-          'Disney Plus Party, Disney Plus Watch Party, Watch Party Disney Plus, How to do a Watch Party on Disney Plus, Disney Watch Party, Disney Party, Watch Party on Disney Plus, How to Host a Disney Plus Watch Party',
+        title: allmeta.homeMetaTitle,
+        description: allmeta.homeMetadescription,
+        keywords: allmeta.homeMetakeywords,
         pageUrl: 'https://www.disneyplusparty.com/',
         featuredImage: '/logo.png',
       },
@@ -23,17 +25,17 @@ export async function getStaticProps() {
   }
 }
 
-const Home = () => {
+const Home = ({ home }) => {
   return (
     <div>
       <Script src="https://cdn.tailwindcss.com" />
 
-      <Navbar />
-      <Banner />
-      <Text />
-      <Text2 />
-      <Fask_ques />
-      <Footer />
+      <Navbar installBtn={home.header.installBtn} />
+      <Banner heading={home.header.navBarHeading} />
+      <Text installBtn={home.header.installBtn} home={home} />
+      <Text2 installBtn={home.header.installBtn} home={home} />
+      <Fask_ques home={home} />
+      <Footer installBtn={home.header.installBtn} footer={home.footer} />
 
       <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></Script>
     </div>
